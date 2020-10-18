353660f5efd922c03b8eb25238dcdc22bc060cf8
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-10-18 16:02:20 -0300

    Updates

---
+++ precache-manifest.23547c33097d98446d63a35c3092e6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0799a4396603bf64b28c5e47a907117f",
+    "url": "/raect-bootstrap-diario/index.html"
+  },
+  {
+    "revision": "8e9c08ab0f991fbd3f18",
+    "url": "/raect-bootstrap-diario/static/css/main.86fda925.chunk.css"
+  },
+  {
+    "revision": "25cebe5aeca2d65456c8",
+    "url": "/raect-bootstrap-diario/static/js/2.9830a8d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/raect-bootstrap-diario/static/js/2.9830a8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9c08ab0f991fbd3f18",
+    "url": "/raect-bootstrap-diario/static/js/main.28762637.chunk.js"
+  },
+  {
+    "revision": "cfa965e2768fea945f32",
+    "url": "/raect-bootstrap-diario/static/js/runtime-main.a577a44f.js"
+  }
+]);
